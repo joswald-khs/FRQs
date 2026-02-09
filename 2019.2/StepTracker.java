c1e4f76cbe9ec55d8605c966b5fc868acc277c3a
Jason L Oswald <joswald@kentshill.org>
2026-02-09 14:59:51 -0500

    Add 2019.2 Structure

---
+++ 2019.2/StepTracker.java
@@ -0,0 +1,24 @@
+
+/**
+ * This question involves the implementation of a fitness tracking system
+ * that is represented by the StepTracker class. A StepTracker object is
+ * created with a parameter that defines the minimum number of steps that
+ * must be taken for a day to be considered active
+ * 
+ * The StepTracker class provides a constructor and the following methods:
+ * 
+ * <ul>
+ * <li>addDailySteps, which accumulates information about steps in readings
+ * taken once per day</li>
+ * <li>activeDays, which returns the number of active days</li>
+ * <li>averageSteps, which returns the average number of steps per day,
+ * calculated by dividing the total number of steps taken by the number of
+ * days tracked</li>
+ * </ul>
+ *
+ * @author The College Board
+ * @version 2019
+ */
+public class StepTracker
+{
+}
