ef508b33e1cb0cae16aadb0f641cb835841cc07a
Jason L Oswald <joswald@kentshill.org>
2026-02-09 15:17:32 -0500

    solved

--- 2019.2/StepTracker.java
+++ 2019.2/StepTracker.java
@@ -1,1 +1,47 @@
+import java.util.ArrayList;
 
+/**
+ * This question involves the implementation of a fitness tracking system
+ * that is represented by the StepTracker class. A StepTracker object is
+ * created with a parameter that defines the minimum number of steps that
+ * must be taken for a day to be considered active
+ * 
+ * The StepTracker class provides a constructor and the following methods:
+ * 
+ * <ul>
+ * <li>addDailySteps, which accumulates information about steps in readings
+ * taken once per day</li>
+ * <li>activeDays, which returns the number of active days</li>
+ * <li>averageSteps, which returns the average number of steps per day,
+ * calculated by dividing the total number of steps taken by the number of
+ * days tracked</li>
+ * </ul>
+ *
+ * @author The College Board
+ * @version 2019
+ */
+public class StepTracker{
+    private ArrayList<Integer> dailySteps = new ArrayList<Integer>();
+    private final int THRESHOLD;
+    private int activeDays = 0;
+    
+    public StepTracker( int threshold ) {
+        THRESHOLD = threshold;
+    }
+    
+    public void addDailySteps( int steps ) {
+        dailySteps.add(steps);
+        if( steps > THRESHOLD ) { activeDays++; } 
+    }
+    
+    public int activeDays() { return activeDays; }
+    
+    public double averageSteps() { 
+        if( dailySteps.size() == 0 ) { return 0; }
+        double average = 0;
+        for( int ds : dailySteps ) {
+            average += ds;
+        }
+        return average / dailySteps.size();
+    }
+}
--- 2019.2/StepTrackerTest.java
+++ 2019.2/StepTrackerTest.java
@@ -1,1 +1,71 @@
 
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class StepTrackerTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class StepTrackerTest
+{
+    StepTracker tr;
+    /**
+     * Default constructor for test class StepTrackerTest
+     */
+    public StepTrackerTest()
+    {
+    }
+
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+        tr = new StepTracker(10000);
+    }
+    
+    @DisplayName("Testing Constructor")
+    @Test    
+    public void testConstructor() {
+        assertEquals( 0, tr.activeDays() );
+        assertEquals( 0.0, tr.averageSteps() );        
+    }
+
+    
+    @DisplayName("First two days")
+    @Nested    
+    class FirstTwoDays {
+        @BeforeEach
+        void day1Entries() {
+            tr.addDailySteps(9000);
+            tr.addDailySteps(5000);
+        }
+        
+        @Test
+        void validate() {
+            assertEquals( 0, tr.activeDays() );
+            assertEquals( 7000.0, tr.averageSteps() );               
+        }
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
